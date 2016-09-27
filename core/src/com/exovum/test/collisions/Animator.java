9dcee5d0b6549b1879ec94e54907ef7b19b74879
Caleb Stevenson <stevecal@mail.gvsu.edu>
2016-09-26 21:52:41 -0400

    Updated Animator.java: Trying out Viewports and Cameras to handle different resolutions.

--- core/src/com/exovum/test/collisions/Animator.java
+++ core/src/com/exovum/test/collisions/Animator.java
@@ -2,11 +2,15 @@ package com.exovum.test.collisions;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by exovu_000 on 9/23/2016.
@@ -25,10 +29,17 @@ public class Animator implements ApplicationListener {
     private SpriteBatch spriteBatch;
     private TextureRegion currentFrame;
 
+    private Viewport viewport;
+    private Camera camera;
+
     float stateTime;
 
     @Override
     public void create() {
+
+        camera = new PerspectiveCamera();
+        viewport = new FitViewport(800, 480, camera);
+
         walkSheet = new Texture(Gdx.files.internal("animation_sheet.png"));
         TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS);
         walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
@@ -45,17 +56,19 @@ public class Animator implements ApplicationListener {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stateTime += Gdx.graphics.getDeltaTime();
         currentFrame = walkAnimation.getKeyFrame(stateTime, true);
         spriteBatch.begin();
         // begin SpriteBatch rendering
-        spriteBatch.draw(currentFrame, 400, 200); //, 256, 307.2f);
+        spriteBatch.draw(currentFrame, 400, 200, 64, 64); //, 256, 307.2f);
         spriteBatch.end();
     }
 
